95f4dff5568b9ee9c4d9d5cbc8bde0386578cc18
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2023-04-27 19:33:52 -0600

    Started appliers functions

---
+++ src/appliers.js
@@ -0,0 +1,13 @@
+import pullUpCurrentList from ****;
+
+export function applyClicksButtons() {
+  const currentList = document.getElementsByClassName('current-list');
+  currentList.addEventListener('click', () => {
+    pullUpCurrentList
+        }
+    );
+}
+
+export function doSomething() {
+  console.log('something went');
+}
